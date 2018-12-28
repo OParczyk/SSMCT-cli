04562190f46afe05601f5c4cbd5015472a4eaafc
Oliver Parczyk <oliver1199@web.de>
2018-12-28 19:19:32 +0100

    Pass on segments after segmentation

--- src/main/java/de/olipar/ssmct/controller/FileSegmentationController.java
+++ src/main/java/de/olipar/ssmct/controller/FileSegmentationController.java
@@ -19,15 +19,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import de.olipar.ssmct.segmentation.Param;
 import de.olipar.ssmct.segmentation.ParameterDisplayType;
 import de.olipar.ssmct.segmentation.Segmenter;
-import de.olipar.ssmct.storage.StorageFileNotFoundException;
 import de.olipar.ssmct.storage.StorageService;
 
 @Controller
@@ -65,9 +67,9 @@ public class FileSegmentationController {
 	}
 
 	@PostMapping(value = "/segment")
-	public String segmentAndCompare(HttpServletRequest request, RedirectAttributes redirectAttrs) {
+	public RedirectView segment(HttpServletRequest request, RedirectAttributes redirectAttrs) {
 		Map<String, String[]> payload = request.getParameterMap();
-
+		Comparable<?>[][] segments;
 		System.out.println(payload.size());
 		for (String i : payload.keySet()) {
 			System.out.println(i + " :");
@@ -81,7 +83,7 @@ public class FileSegmentationController {
 			redirectAttrs.addAttribute("message",
 					"Error processing request: " + payload.get("segmenter")[0] + " not Found!");
 			e1.printStackTrace();
-			return ("redirect:/segmentation");
+			return new RedirectView("segmentation");
 		}
 		Constructor<? extends Segmenter<? extends Comparable<?>>>[] ctors = (Constructor<? extends Segmenter<? extends Comparable<?>>>[]) clazz
 				.getConstructors();
@@ -107,7 +109,7 @@ public class FileSegmentationController {
 			String payloadContent = payload.get(i.getName())[0];
 			switch (i.getAnnotation(Param.class).type()) {
 			case BOOLEAN:
-				if (payload.get(i.getName()).length>1)
+				if (payload.get(i.getName()).length > 1)
 					params.add(new Boolean(true));
 				else
 					params.add(new Boolean(false));
@@ -126,7 +128,6 @@ public class FileSegmentationController {
 				break;
 			}
 		}
-		Comparable[][] segments;
 		Path filepath = storageService.load(payload.get("filename")[0]);
 		// Create an instance of requested segmenter
 		try {
@@ -137,15 +138,15 @@ public class FileSegmentationController {
 				| InvocationTargetException e) {
 			redirectAttrs.addAttribute("message", "Error parsing given Parameters");
 			e.printStackTrace();
-			return ("redirect:/segmentation");
+			return new RedirectView("segmentation");
 		} catch (IOException e) {
 			redirectAttrs.addAttribute("message", "Error reading given file");
 			e.printStackTrace();
-			return ("redirect:/segmentation");
+			return new RedirectView("segmentation");
 		}
-		
-		redirectAttrs.addAttribute("segments", segments);
 
-		return ("redirect:/compare");
+		request.getSession().setAttribute("segments", segments);
+		System.out.println(request.getSession().getAttribute("segments"));
+		return new RedirectView("compare", false);
 	}
 }
